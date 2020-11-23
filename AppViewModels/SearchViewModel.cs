18f96ab82a70daf13227e748eec15b0c73e5f77d
Seena Mary  Augusty <seenamaryaugusty@gmail.com>
2020-11-24 00:20:43 +0530

    Removed unncessary refernces

--- AppViewModels/SearchViewModel.cs
+++ AppViewModels/SearchViewModel.cs
@@ -1,14 +1,7 @@
 ﻿using ModelLibraries;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Input;
 using AppModels;
 using System.Collections.ObjectModel;
-using System.Windows.Data;
-using System.Xml.Linq;
 
 namespace AppViewModels
 {
